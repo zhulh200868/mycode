542f6635b1a68c4dfcf4cb6229ece0d0ffe28127
zhulh200868 <3205777381@qq.com>
2016-10-22 16:54:25 +0800

    mycode

---
+++ multiprocess_demo.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding=utf8 -*-
+
+from multiprocessing import Pool
+import time
+
+
+def test(num):
+    time.sleep(10)
+    print(num)
+
+
+if __name__ == "__main__":
+    pool = Pool(1)
+    for i in range(10):
+        pool.apply_async(func=test,args=(i,))
+    while True:
+        for i in range(100):
+            time.sleep(5)
+            print "num --> %s"%i
+        break
+    pool.close()
+    pool.join()
+
+
