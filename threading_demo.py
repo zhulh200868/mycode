85a8b655e2de1b42cf209e8f8d3f7082bd437e00
zhulh200868 <3205777381@qq.com>
2016-10-22 16:58:20 +0800

    mycode

---
+++ threading_demo.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding=utf8 -*-
+
+# http://www.cnblogs.com/alex3714/articles/5230609.html
+
---
+++ time_demo.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding=utf8 -*-
+
+"""
+二、获取当前时间：
+# >>> import time
+# >>> time.strftime('%Y-%m-%d %H',time.localtime(time.time()))
+'2016-07-06 18'
+"""
+"""
+# 三、使用split使用多个字符分隔字符串：
+# >>> a='Beautiful, is; better*than\nugly'
+# >>>import re
+# >>> re.split('; |, |\*|\n',a)
+# ['Beautiful','is','better','than','ugly']
+# 四、计算两个带日期的时间差：
+# def Caltime(date1,date2):
+#     date1=time.strptime(date1,"%Y-%m-%d %H:%M:%S")
+#     date2=time.strptime(date2,"%Y-%m-%d %H:%M:%S")
+#     date1=datetime.datetime(date1[0],date1[1],date1[2],date1[3],date1[4],date1[5])
+#     date2=datetime.datetime(date2[0],date2[1],date2[2],date2[3],date2[4],date2[5])
+#     return date2-date1
+"""
