784e19a22e86de5415a0293072bfaedbdcd7bbbf
zhulh200868 <3205777381@qq.com>
2016-11-08 11:27:08 +0800

    mycode

--- Aircraftcarrier/Ronaldo/views.py
+++ Aircraftcarrier/Ronaldo/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render,render_to_response,HttpResponseRedirect,Http
 from django.template import RequestContext
 from django import forms
 from models import User
+import logger
 
 # Create your views here.
 
@@ -17,35 +18,36 @@ class UserForm(forms.Form):
 def home(request):
     username = request.COOKIES.get('username','')
     if username:
+        logger.logger.info("%s loggin !"%username)
         return render_to_response('index.html',{'username':username})
     else:
          return render_to_response('login.html')
 
 
 def autodeploy(request):
-    return render_to_response('index2.html')
+    return render_to_response('autodeploy.html')
 
 
 
-#注册
-def regist(request):
-    if request.method == 'POST':
-        form_value = UserForm(request.POST)
-        if form_value.is_valid():
-            #获得表单数据
-            username = form_value.cleaned_data['username']
-            password = form_value.cleaned_data['password']
-            #添加到数据库
-            User.objects.create(username= username,password=password)
-            # return HttpResponse('regist success!!')
-            return render_to_response('login.html')
-    else:
-        form_value = UserForm()
-    return render_to_response('regist.html',{'form_value':form_value}, context_instance=RequestContext(request))
+# #注册
+# def regist(request):
+#     if request.method == 'POST':
+#         form_value = UserForm(request.POST)
+#         if form_value.is_valid():
+#             #获得表单数据
+#             username = form_value.cleaned_data['username']
+#             password = form_value.cleaned_data['password']
+#             #添加到数据库
+#             User.objects.create(username= username,password=password)
+#             # return HttpResponse('regist success!!')
+#             logger.logger.info("%s regist success !"%username)
+#             return render_to_response('login.html')
+#     else:
+#         form_value = UserForm()
+#     return render_to_response('regist.html',{'form_value':form_value}, context_instance=RequestContext(request))
 
 #登陆
 def login(request):
-    print request.POST
     if request.method == 'POST':
         form_value = UserForm(request.POST)
         if form_value.is_valid():
@@ -59,6 +61,7 @@ def login(request):
                 response = HttpResponseRedirect('/cmdb/home/')
                 #将username写入浏览器cookie,失效时间为3600
                 response.set_cookie('username',username,3600)
+                # logger.logger.info("%s loggin !"%username)
                 return response
             else:
                 #比较失败，还在login
@@ -70,7 +73,7 @@ def login(request):
 
 #退出
 def logout(request):
-    response = HttpResponse('logout !!')
+    response = HttpResponseRedirect('/cmdb/login/')
     #清理cookie里保存username
     response.delete_cookie('username')
     return response
